7ebada0a1646f8352c10bca3511f6fa4325994fb
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-04-21 01:05:10 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+  name : "Malik",
+  streetAddress : "15 Bob"
+};
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+  const newObject = {...employee};
+  newObject[key]=value;
+  return newObject;
+}
+
+console.log(updateEmployeeWithKeyAndValue(employee, "name", "James"));
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  const newObject = employee;
+  newObject[key]=value;
+  return newObject;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+  const newObject = {...employee};
+  delete newObject[key];
+  return newObject;
+}
+
+console.log(destructivelyDeleteFromEmployeeByKey(employee,"name"));
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+  const newObject = employee;
+  delete newObject[key];
+  return newObject;
+}
